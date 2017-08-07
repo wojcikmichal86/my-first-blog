1270ead913037773f6b3d0708e622ba16678c37b
wojcikmichal86 <wojcik.michal86@gmail.com>
2017-08-07 22:47:54 +0200

    Adding gyms

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^academias/$', views.academias, name='academias'),
     url(r'', include('blog.urls')),
 
 ]
