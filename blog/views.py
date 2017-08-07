440dc3fc061df81719791a9094cc130c99754955
wojcikmichal86 <wojcik.michal86@gmail.com>
2017-08-07 22:24:29 +0200

    Adding gyms

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post, Comment
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+import requests
+
+from bs4 import BeautifulSoup
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -13,6 +17,63 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def academias(request):
+    return HttpResponse(display+'</table>')
+
+gyms=['lea', 'mogilska', 'solvaypark', 'wadowicka', 'krakowska', 'aleksandry', 'plaza', 'zakopianska', 'nastoku', 'bratyslawska']
+
+display='<table style="width:100%"><tr><th>Aula</th><th>Dia</th><th>Hora</th><th>Academia</th></tr>'
+def gym_spider(gym):
+        global display
+        godziny = []
+        zajecia = []
+        url = 'http://fitnessplatinium.pl/'+gym+'/grafik/'
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, "lxml")
+        dni=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+
+        for hour in soup.findAll('td', {'class': 'hour'}):
+            if hour.contents==[]:
+                hour.contents=['']
+            godziny.append(hour.contents)
+        for n,i in enumerate(godziny):
+            if i==['']:
+                godziny[n]=godziny[n-1]
+        temp=[]
+        for a in godziny:
+            if isinstance(a, list):
+                temp.extend(a)
+
+
+        for aula in soup.findAll('td', {'class': 'active'}):
+            if aula.contents==None:
+                aula.contents=[]
+            else:
+                aula.contents=str(aula.findAll('h6'))[5:-6]
+            zajecia.append([aula.contents])
+
+        for c in zajecia:
+            if "&amp" in c[0]:
+                c[0]=c[0].replace("&amp;","&")
+            c.append(dni[zajecia.index(c)%7])
+            c.append(str(temp[int(zajecia.index(c)/7)]))
+            c.append(gym)
+        klasy=[]
+        for c in zajecia:
+            if c[0]!='':
+                klasy.append(c)
+        for klasa in klasy:
+            display+='<tr>'
+            for info in klasa:
+                display+='<td style="text-align:center">'+str(info)+'</td>'
+            display+='</tr>'
+
+for gym in gyms:
+    gym_spider(gym)
+
 
 @login_required
 def post_new(request):
