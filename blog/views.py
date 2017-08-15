7ab37c5f9861b71e39fc00e46c7d0431313eb093
wojcikmichal86 <wojcik.michal86@gmail.com>
2017-08-15 16:37:23 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
-from urllib3 import requests
+from fequests import requests
 
 from bs4 import BeautifulSoup
 
