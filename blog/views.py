cb59c6b9abcb5b198c6d75cd6a08a3b574ef15b1
wojcikmichal86 <wojcik.michal86@gmail.com>
2017-08-15 17:22:53 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
-import urllib3
+import requests
 from bs4 import BeautifulSoup
 
 def post_list(request):
@@ -24,52 +24,48 @@ gyms=['lea', 'mogilska', 'solvaypark', 'wadowicka', 'krakowska', 'aleksandry', '
 
 display='<table style="width:100%"><tr><th>Aula</th><th>Dia</th><th>Hora</th><th>Academia</th></tr>'
 def gym_spider(gym):
-        global display
-        godziny = []
-        zajecia = []
-        url = 'http://fitnessplatinium.pl/'+gym+'/grafik/'
-        source_code = urllib3.PoolManager()
-        plain_text = source_code.request('GET', url)
-        soup = BeautifulSoup(plain_text, "lxml")
-        dni=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    global display
+    godziny = []
+    zajecia = []
+    url = 'http://fitnessplatinium.pl/'+gym+'/grafik/'
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "lxml")
+    dni=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 
-        for hour in soup.findAll('td', {'class': 'hour'}):
-            if hour.contents==[]:
-                hour.contents=['']
-            godziny.append(hour.contents)
-        for n,i in enumerate(godziny):
-            if i==['']:
-                godziny[n]=godziny[n-1]
-        temp=[]
-        for a in godziny:
-            if isinstance(a, list):
-                temp.extend(a)
-
-
-        for aula in soup.findAll('td', {'class': 'active'}):
-            if aula.contents==None:
-                aula.contents=[]
-            else:
-                aula.contents=str(aula.findAll('h6'))[5:-6]
-            zajecia.append([aula.contents])
-
-        for c in zajecia:
-            if "&amp" in c[0]:
-                c[0]=c[0].replace("&amp;","&")
-            c.append(dni[zajecia.index(c)%7])
-            c.append(str(temp[int(zajecia.index(c)/7)]))
-            c.append(gym)
-        klasy=[]
-        for c in zajecia:
-            if c[0]!='':
-                klasy.append(c)
-        for klasa in klasy:
-            display+='<tr>'
-            for info in klasa:
-                display+='<td style="text-align:center">'+str(info)+'</td>'
-            display+='</tr>'
-
+    for hour in soup.findAll('td', {'class': 'hour'}):
+        if hour.contents==[]:
+            hour.contents=['']
+        godziny.append(hour.contents)
+    for n,i in enumerate(godziny):
+        if i==['']:
+            godziny[n]=godziny[n-1]
+    temp=[]
+    for a in godziny:
+        if isinstance(a, list):
+            temp.extend(a)
+    for aula in soup.findAll('td', {'class': 'active'}):
+        if aula.contents==None:
+            aula.contents=[]
+        else:
+            aula.contents=str(aula.findAll('h6'))[5:-6]
+        zajecia.append([aula.contents])
+    for c in zajecia:
+        if "&amp" in c[0]:
+            c[0]=c[0].replace("&amp;","&")
+        c.append(dni[zajecia.index(c)%7])
+        c.append(str(temp[int(zajecia.index(c)/7)]))
+        c.append(gym)
+    klasy=[]
+    for c in zajecia:
+        if c[0]!='':
+            klasy.append(c)
+    for klasa in klasy:
+        display+='<tr>'
+        for info in klasa:
+            display+='<td style="text-align:center">'+str(info)+'</td>'
+        display+='</tr>'
 for gym in gyms:
     gym_spider(gym)
 
